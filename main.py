df4444a15de703912c709e5e3820df57fb2e69a2
Tarcadia <tarcadia@qq.com>
2021-11-12 11:52:41 +0800

    现在还没积重难返，我也不知道怎么命名比较符合传统，但是这里的logger是一个主要部件，就不要__吧

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
-import logging as __logging__;
+import logging;
 
 VERSION = 'v20211111';
 
-__logger__ = __logging__.getLogger(__name__);
-__logger__.setLevel(__logging__.DEBUG);
-__logger_ch__ = __logging__.StreamHandler();
-__logger_ch__.setLevel(__logging__.DEBUG);
-__logger_formatter__ = __logging__.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m[%(name)s]\033[0m >> %(message)s', datefmt='%H:%M');
-__logger_ch__.setFormatter(__logger_formatter__);
-__logger__.addHandler(__logger_ch__);
-__logger__.info('Running...');
+logger = logging.getLogger(__name__);
+logger.setLevel(logging.DEBUG);
+_logger_ch = logging.StreamHandler();
+_logger_ch.setLevel(logging.DEBUG);
+_logger_formatter = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m[%(name)s]\033[0m >> %(message)s', datefmt='%H:%M');
+_logger_ch.setFormatter(_logger_formatter);
+logger.addHandler(_logger_ch);
+logger.info('Running...');
 
 
 
@@ -23,6 +23,6 @@ try:
     with serv.open() as updates:
         for update in updates:
             if update:
-                __logger__.debug(update);
+                logger.debug(update);
 except:
-    __logger__.info('Ended.');
+    logger.info('Ended.');
