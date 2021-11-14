7933004f1610c0b4e9b80f55643a082d8f378d5a
Tarcadia <tarcadia@qq.com>
2021-11-15 04:33:44 +0800

    根据Shell的整理重新整理了User，虽然啥都没实现

--- BNYYCS/bnyycsUser.py
+++ BNYYCS/bnyycsUser.py
@@ -15,24 +15,17 @@ __all__ = [
 class User:
 
     def __init__(self) -> None:
-        self.res = Res.Res_RefusePage();
         self.tab = -1;
         self.cmd = '';
-        self.arg = {};
+        self.cmds = [];
         self._cmd_ready = False;
         return;
     
-    @property
-    def page(self):
-        #return CHR_CLR + CHR_T_FC_BLUE + b"Hello World" + CHR_T_RST + CHR_CRLF;
-        return self.res.draw(self.tab);
-    
-    @property
-    def line(self):
+    def draw(self, res, **kwargs):
         return b"\033[0;33m >> \033[0;34m%s\033[0m" % bytes(self.cmd, "ascii");
-    
-    def update(self, inp):
-        for c in inp:
-            self.tab = (self.tab + 2) % (len(self.res.cmds) + 1) - 1;
-            self.cmd = self.res.cmds[self.tab] if self.tab >= 0 else self.cmd;
+
+    def update(self, recv, **kwargs):
+        for c in recv:
+            self.tab = (self.tab + 2) % (len(self.cmds) + 1) - 1;
+            self.cmd = self.cmds[self.tab] if self.tab >= 0 else self.cmd;
         return None;
