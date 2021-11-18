b21b939232fe07e973643b3562f767d508a56951
Tarcadia <tarcadia@qq.com>
2021-11-19 04:05:51 +0800

    模块名显示

--- BNYYCS/bnyycsLog.py
+++ BNYYCS/bnyycsLog.py
@@ -8,7 +8,7 @@ LOG_FILE = './Prototype.BNYYCS.log';
 logger = logging.getLogger("BNYYCS");
 logger.setLevel(logging.DEBUG);
 
-_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> \033[0m%(message)s', datefmt='%H:%M');
+_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[35m[%(name)s]\033[33m >> \033[0m%(message)s', datefmt='%H:%M');
 _logger_ch_scrn = logging.StreamHandler();
 _logger_ch_scrn.setLevel(logging.INFO);
 _logger_ch_scrn.setFormatter(_logger_formatter_scrn);
