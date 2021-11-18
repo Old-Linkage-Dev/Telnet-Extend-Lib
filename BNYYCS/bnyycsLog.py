21c680dbee6bd57635a159eeb7fa755b7e096511
Tarcadia <tarcadia@qq.com>
2021-11-19 03:57:39 +0800

    Better log display

--- BNYYCS/bnyycsLog.py
+++ BNYYCS/bnyycsLog.py
@@ -3,13 +3,20 @@
 
 import logging;
 
+LOG_FILE = './Prototype.BNYYCS.log';
+
 logger = logging.getLogger("BNYYCS");
 logger.setLevel(logging.DEBUG);
-_logger_ch = logging.StreamHandler();
-_logger_ch.setLevel(logging.DEBUG);
-_logger_formatter = logging.Formatter(
-    fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m[%(name)s]\033[0m >> %(message)s',
-    datefmt='%H:%M'
-);
-_logger_ch.setFormatter(_logger_formatter);
-logger.addHandler(_logger_ch);
+
+_logger_formatter_scrn = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m >> \033[0m%(message)s', datefmt='%H:%M');
+_logger_ch_scrn = logging.StreamHandler();
+_logger_ch_scrn.setLevel(logging.INFO);
+_logger_ch_scrn.setFormatter(_logger_formatter_scrn);
+
+_logger_formatter_file = logging.Formatter(fmt='[%(asctime)s][%(levelname)s] >> %(message)s', datefmt='%Y-%m-%d-%H:%M:%S');
+_logger_ch_file = logging.FileHandler(LOG_FILE, encoding = 'utf8');
+_logger_ch_file.setLevel(logging.DEBUG);
+_logger_ch_file.setFormatter(_logger_formatter_file);
+
+logger.addHandler(_logger_ch_scrn);
+logger.addHandler(_logger_ch_file);
