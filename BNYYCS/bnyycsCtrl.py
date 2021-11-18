d3b046b1cb30319126d884baa54efb54c8784c89
Tarcadia <tarcadia@qq.com>
2021-11-18 22:29:00 +0800

    根据RFC文档实现更多处理 由于IAC SB后会跟随对应OPCODE，这里不需要再将其于IAC DO/WILL合并 在可以遇见的范围内避免了commit efa36be提到的冲突

--- BNYYCS/bnyycsCtrl.py
+++ BNYYCS/bnyycsCtrl.py
@@ -330,6 +330,10 @@ T_BRST              = 49;
 
 # Telnet 控制字符
 TEL_NULL            = b'\x00';
+TEL_EOF             = b'\xEC';
+TEL_SUSP            = b'\xED';
+TEL_ABORT           = b'\xEE';
+TEL_EOR             = b'\xEF';
 TEL_SE              = b'\xF0';
 TEL_NOP             = b'\xF1';
 TEL_DM              = b'\xF2';
@@ -345,11 +349,101 @@ TEL_WILL            = b'\xFB';
 TEL_WONT            = b'\xFC';
 TEL_DO              = b'\xFD';
 TEL_DONT            = b'\xFE';
+TEL_xFF             = b'\xFF';
 TEL_IAC             = b'\xFF';
 
+# RFC 656
+TEL_OP_TB           = b'\x00';
+# RFC 857
 TEL_OP_ECHO         = b'\x01';
+# RFC 858
 TEL_OP_SPRGA        = b'\x03';
+# RFC 859
+TEL_OP_STA          = b'\x05';
+TEL_OP_STA_IS           = b'\x00';
+TEL_OP_STA_SEND         = b'\x01';
+# RFC 860
+TEL_OP_TM           = b'\x06';
+# RFC 884 930 1091
+TEL_OP_TTYP         = b'\x18';
+TEL_OP_TTYP_IS          = b'\x00';
+TEL_OP_TTYP_SEND        = b'\x01';
+# RFC 1073
 TEL_OP_NAWS         = b'\x1F';
+# RFC 884 930 1079
+TEL_OP_TSPD         = b'\x20';
+TEL_OP_TSPD_IS          = b'\x00';
+TEL_OP_TSPD_SEND        = b'\x01';
+# RFC 1080 1372
+TEL_OP_TOGFC        = b'\x21';
+TEL_OP_TOGFC_OFF        = b'\x21';
+TEL_OP_TOGFC_ON         = b'\x21';
+TEL_OP_TOGFC_RSTANY     = b'\x21';
+TEL_OP_TOGFC_RSTXON     = b'\x21';
+# RFC 1116 1184
+TEL_OP_LNMOD        = b'\x22';
+TEL_OP_LNMOD_MODE       = b'\x01';
+TEL_OP_LNMOD_EDIT       = b'\x01';
+TEL_OP_LNMOD_TRAPSIG        = b'\x02';
+TEL_OP_LNMOD_MODEACK        = b'\x04';
+TEL_OP_LNMOD_SOFTTAB        = b'\x08';
+TEL_OP_LNMOD_LITECHO        = b'\x10';
+TEL_OP_LNMOD_FWDMSK     = b'\x02';
+TEL_OP_LNMOD_SLC        = b'\x03';
+TEL_OP_LNMOD_SLC_SYNCH      = b'\x01';
+TEL_OP_LNMOD_SLC_BRK        = b'\x02';
+TEL_OP_LNMOD_SLC_IP         = b'\x03';
+TEL_OP_LNMOD_SLC_AO         = b'\x04';
+TEL_OP_LNMOD_SLC_AYT        = b'\x05';
+TEL_OP_LNMOD_SLC_EOR        = b'\x06';
+TEL_OP_LNMOD_SLC_ABORT      = b'\x07';
+TEL_OP_LNMOD_SLC_EOF        = b'\x08';
+TEL_OP_LNMOD_SLC_SUSP       = b'\x09';
+TEL_OP_LNMOD_SLC_EC         = b'\x0A';
+TEL_OP_LNMOD_SLC_EL         = b'\x0B';
+TEL_OP_LNMOD_SLC_EW         = b'\x0C';
+TEL_OP_LNMOD_SLC_RP         = b'\x0D';
+TEL_OP_LNMOD_SLC_LNEXT      = b'\x0E';
+TEL_OP_LNMOD_SLC_XON        = b'\x0F';
+TEL_OP_LNMOD_SLC_XOFF       = b'\x10';
+TEL_OP_LNMOD_SLC_FORW1      = b'\x11';
+TEL_OP_LNMOD_SLC_FORW2      = b'\x12';
+TEL_OP_LNMOD_SLC_MCL        = b'\x13';
+TEL_OP_LNMOD_SLC_MCR        = b'\x14';
+TEL_OP_LNMOD_SLC_MCWL       = b'\x15';
+TEL_OP_LNMOD_SLC_MCWR       = b'\x16';
+TEL_OP_LNMOD_SLC_MCBOL      = b'\x17';
+TEL_OP_LNMOD_SLC_MCEOL      = b'\x18';
+TEL_OP_LNMOD_SLC_INSRT      = b'\x19';
+TEL_OP_LNMOD_SLC_OVER       = b'\x1A';
+TEL_OP_LNMOD_SLC_ECR        = b'\x1B';
+TEL_OP_LNMOD_SLC_EWR        = b'\x1C';
+TEL_OP_LNMOD_SLC_EBOL       = b'\x1D';
+TEL_OP_LNMOD_SLC_EEOL       = b'\x1E';
+TEL_OP_LNMOD_SLC_DEFAUL     = b'\x03';
+TEL_OP_LNMOD_SLC_VALUE      = b'\x02';
+TEL_OP_LNMOD_SLC_CNTCHG     = b'\x01';
+TEL_OP_LNMOD_SLC_NOSPRT     = b'\x00';
+TEL_OP_LNMOD_SLC_LVLBIT     = b'\x03';
+TEL_OP_LNMOD_SLC_ACK        = b'\x80';
+TEL_OP_LNMOD_SLC_FLSIN      = b'\x40';
+TEL_OP_LNMOD_SLC_FLSOUT     = b'\x20';
+TEL_OP_LNMOD_EOF        = b'\xEC';
+TEL_OP_LNMOD_SUSP       = b'\xED';
+TEL_OP_LNMOD_ABORT      = b'\xEE';
+# RFC 1408 1517
+TEL_OP_ENVIRON      = b'\x24';
+TEL_OP_ENVIRON_IS       = b'\x00';
+TEL_OP_ENVIRON_SEND     = b'\x01';
+TEL_OP_ENVIRON_INFO     = b'\x02';
+TEL_OP_ENVIRON_VAR      = b'\x00';
+TEL_OP_ENVIRON_VALU     = b'\x01';
+TEL_OP_ENVIRON_ESC      = b'\x02';
+TEL_OP_ENVIRON_USEV     = b'\x03';
+# RFC 748 you know
+TEL_OP_RANDLOS      = b'\x100';
+# RFC 1097 you know
+TEL_OP_SUBLIMMSG    = b'\x101';
 
 TEL_CMD_SE          = TEL_IAC + TEL_SE;
 TEL_CMD_NOP         = TEL_IAC + TEL_NOP;
@@ -366,20 +460,46 @@ TEL_CMD_WILL        = TEL_IAC + TEL_WILL;
 TEL_CMD_WONT        = TEL_IAC + TEL_WONT;
 TEL_CMD_DO          = TEL_IAC + TEL_DO;
 TEL_CMD_DONT        = TEL_IAC + TEL_DONT;
-
-TELS_OPFORE         = (TEL_CMD_WILL, TEL_CMD_WONT, TEL_CMD_DO, TEL_CMD_DONT);
-TELS_OPWITHSUB      = (TEL_CMD_WILL + TEL_OP_NAWS, );
+TEL_CMD_xFF         = TEL_IAC + TEL_xFF;
 
 def TELf_CMD(cmd):
     assert type(cmd) == bytes;
     return TEL_IAC + cmd;
 
-def TELf_SB(*args):
-    _ret = TEL_IAC + TEL_SB;
+def TELf_WILL(cmd):
+    assert type(cmd) == bytes;
+    return TEL_CMD_WILL + cmd;
+
+def TELf_WONT(cmd):
+    assert type(cmd) == bytes;
+    return TEL_CMD_WONT + cmd;
+
+def TELf_DO(cmd):
+    assert type(cmd) == bytes;
+    return TEL_CMD_DO + cmd;
+
+def TELf_DONT(cmd):
+    assert type(cmd) == bytes;
+    return TEL_CMD_DONT + cmd;
+
+def TELf_SB(cmd, *args):
+    assert type(cmd) == bytes;
+    _ret = TEL_IAC + TEL_SB + cmd;
     for _val in args:
         assert type(_val) == bytes;
         _ret += _val;
     _ret += TEL_IAC + TEL_SE;
+
+TELS_OPFORE         = (TEL_CMD_WILL, TEL_CMD_WONT, TEL_CMD_DO, TEL_CMD_DONT);
+TELS_SUBOPLEN       = {
+    TEL_IAC + TEL_SB + TEL_OP_NAWS : 3 + 4 + 2,
+    TEL_IAC + TEL_SB + TEL_OP_STA : -1,
+    TEL_IAC + TEL_SB + TEL_OP_TTYP : -1,
+    TEL_IAC + TEL_SB + TEL_OP_TSPD : -1,
+    TEL_IAC + TEL_SB + TEL_OP_TOGFC : -1,
+    TEL_IAC + TEL_SB + TEL_OP_LNMOD : -1,
+    TEL_IAC + TEL_SB + TEL_OP_ENVIRON : -1,
+};
 
 
 
@@ -408,22 +528,23 @@ class TelnetInputQueue:
         else:
             if self._input[:2] in TELS_OPFORE and len(self._input) < 3:
                 return b'';
-            elif self._input[:2] in TELS_OPFORE and self._input[:3] not in TELS_OPWITHSUB:
+            elif self._input[:2] in TELS_OPFORE and len(self._input) >= 3:
                 return self.popchar(3);
-            elif self._input[:2] in TELS_OPFORE and self._input[:3] in TELS_OPWITHSUB and len(self._input) < 5:
+            elif self._input[:2] == TEL_CMD_SB and len(self._input) < 5:
                 return b'';
-            elif self._input[:2] in TELS_OPFORE and self._input[:3] in TELS_OPWITHSUB and self._input[3:5] != TEL_CMD_SB:
-                return self.popchar(3);
-            elif self._input[:2] in TELS_OPFORE and self._input[:3] in TELS_OPWITHSUB and self._input[3:5] == TEL_CMD_SB:
-                _i = 5;
-                while _i + 1 < len(self._input) and self._input[_i:_i+2] != TEL_CMD_SE:
-                    _i += 1;
-                return self.popchar(_i + 1);
-            elif self._input[:2] == TEL_CMD_SB:
-                _i = 2;
-                while _i + 1 < len(self._input) and self._input[_i:_i+2] != TEL_CMD_SE:
-                    _i += 1;
-                return self.popchar(_i + 1);
+            elif self._input[:2] == TEL_CMD_SB and len(self._input) >= 5:
+                _sublen = TELS_SUBOPLEN[self._input[:3]];
+                if _sublen == -1:
+                    _i = 3;
+                    while _i + 1 < len(self._input) and self._input[_i : _i + 2] != TEL_CMD_SE:
+                        _i += 1;
+                    return self.popchar(_i + 2);
+                elif len(self._input) < _sublen:
+                    return b'';
+                elif len(self._input) >= _sublen and self._input[_sublen - 2 : _sublen] == TEL_CMD_SE:
+                    return self.popchar(_sublen);
+                else:
+                    return self.popchar(3);
             else:
                 return self.popchar(2);
     
