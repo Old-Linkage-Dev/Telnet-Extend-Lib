ad804080ebfc0c62dcb3091dd62992a4cf0ef1a9
Tarcadia <tarcadia@qq.com>
2021-11-19 04:09:02 +0800

    main的log展示修正

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import logging;
 
 VERSION = 'v20211111';
 
-logger = logging.getLogger(__name__);
+logger = logging.getLogger('MAIN');
 logger.setLevel(logging.DEBUG);
 _logger_ch = logging.StreamHandler();
 _logger_ch.setLevel(logging.DEBUG);
-_logger_formatter = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;33m[%(name)s]\033[0m >> %(message)s', datefmt='%H:%M');
+_logger_formatter = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[33m[%(name)s]\033[33m >> \033[0m%(message)s', datefmt='%H:%M');
 _logger_ch.setFormatter(_logger_formatter);
 logger.addHandler(_logger_ch);
 logger.info('Running...');
