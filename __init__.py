46d55428b3c4b8ca829816551aa7aa9e2886b0d8
Tarcadia <tarcadia@qq.com>
2021-11-23 01:31:45 +0800

    子模块在包结构里的表达

--- __init__.py
+++ __init__.py
@@ -5,7 +5,10 @@
 
 # 该包的总接口，应当对包的细节进行过滤，提供良好的用户调用接口的总结；
 
-from . import *;
+from . import CONSTS;
+from . import TELMain;
+from . import TELShell;
+from . import TELFront;
 
 from .TELMain import *;
 from .TELShell import *;
@@ -16,6 +19,12 @@ from .TELLog import logger;
 
 
 __all__ = (
+    [
+        "CONSTS",
+        "TELMain",
+        "TELShell",
+        "TELFront",
+    ] +
     TELMain.__all__ +
     TELShell.__all__ +
     TELFront.__all__
